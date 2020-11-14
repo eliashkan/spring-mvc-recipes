848341a3c295a622b68aac0a611c1e1d613c8897
Elias Khan <khan.elias@gmail.com>
2020-11-14 17:32:48 +0100

    Initial commit

---
+++ src/main/java/com/realdolmen/springmvcrecipes/SpringMvcRecipesApplication.java
@@ -0,0 +1,13 @@
+package com.realdolmen.springmvcrecipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcRecipesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcRecipesApplication.class, args);
+    }
+
+}
